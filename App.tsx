d2ccc9a767837241b83bdbd5a17d9982f197d45a
Gabriel Pires <gabpiresdev@gmail.com>
2023-01-16 21:36:05 -0300

    setting up custom fonts and loading component

--- App.tsx
+++ App.tsx
@@ -1,7 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from '@expo-google-fonts/inter';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [loaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
+
+  if (!loaded) {
+    return <Loading />;
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>
---
+++ src/components/Loading.tsx
@@ -0,0 +1,15 @@
+import { ActivityIndicator, View } from 'react-native';
+
+export const Loading = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <ActivityIndicator color="#7c3aed" />
+    </View>
+  );
+};
