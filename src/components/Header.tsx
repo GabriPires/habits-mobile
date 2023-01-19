74e42953a20b826a02f45d3feeba070053538e23
Gabriel Pires <gabpiresdev@gmail.com>
2023-01-19 20:34:14 -0300

    navigate to new habit screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,11 @@ import { Feather } from '@expo/vector-icons';
 import colors from 'tailwindcss/colors';
 
 import Logo from '../assets/logo.svg';
+import { useNavigation } from '@react-navigation/native';
 
 export const Header = () => {
+  const { navigate } = useNavigation();
+
   return (
     <View className="w-full flex-row items-center justify-between">
       <Logo />
@@ -12,6 +15,7 @@ export const Header = () => {
       <TouchableOpacity
         activeOpacity={0.7}
         className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
+        onPress={() => navigate('new')}
       >
         <Feather name="plus" color={colors.violet[500]} size={20} />
         <Text className="text-white ml-3 font-semibold text-base">Novo</Text>
--- src/routes/app-routes.tsx
+++ src/routes/app-routes.tsx
@@ -8,7 +8,11 @@ import { Habit } from '../screens/Habit';
 
 export const AppRoutes = () => {
   return (
-    <Navigator>
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
       <Screen name="home" component={Home} />
       <Screen name="new" component={New} />
       <Screen name="habit" component={Habit} />
